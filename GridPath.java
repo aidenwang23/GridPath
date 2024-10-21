e9aaf63959adf6474ae194079d64fad9daf394ad
aidenwang23 <148901698+aidenwang23@users.noreply.github.com>
2024-10-21 15:47:06 +0000

    modified:   GridPath.java 	modified:   Main.java

--- GridPath.java
+++ GridPath.java
@@ -4,7 +4,25 @@ public class GridPath
      * Initialized in the constructor with distinct values that never change
      */
     private int[][] grid;
-    private int sum;
+
+    public GridPath(int[][] values)
+    {
+        grid = values;
+    }
+
+    public String toString()
+    {
+        String s = "";
+        for(int row = 0; row < grid.length; row++)
+        {
+            for (int col = 0; col < grid[0].length; col++)
+            {
+                s += grid[row][col] + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
 
     /**
      * Returns the Location representing a neighbor of the grid element at row
@@ -14,7 +32,23 @@ public class GridPath
      */
     public Location getNextLoc(int row, int col) 
     {
-        return(new Location(row, col));
+        int belowElement = 999;
+        int rightElement = 999;
+        if(row+1 <= grid.length)
+        {
+            rightElement = grid[row+1][col];
+        }
+        if(col+1 <= grid[0].length)
+        {
+            belowElement = grid[row][col+1];
+        }
+        if(belowElement < rightElement){
+            return(new Location(row+1,col));
+        }
+        if(rightElement < belowElement){
+            return(new Location(row,col+1));
+        }
+        return null;
     }
 
     /**
@@ -25,6 +59,8 @@ public class GridPath
      */
     public int sumPath(int row, int col) 
     {
+        int sum = 0;
+
         return sum;
     }
 }
--- Main.java
+++ Main.java
@@ -2,10 +2,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-        GridPath grid = new GridPath();
-        System.out.println(grid.getNextLoc(0,0));
-        System.out.println(grid.getNextLoc(1,3));
-        System.out.println(grid.getNextLoc(2,4));
-        System.out.println(grid.getNextLoc(4,3));
+        int[][] nums = {
+            {12, 3, 4, 13, 5},
+            {11, 21, 2, 14, 16},
+            {7, 8, 9, 15, 0},
+            {10, 17, 20, 19, 1},
+            {18, 22, 30, 25, 6}
+        };
+        GridPath one = new GridPath(nums);
+        System.out.println(one);
+        System.out.println(one.getNextLoc(0,0));
+        System.out.println(one.getNextLoc(1,3));
+        System.out.println(one.getNextLoc(2,4));
+        System.out.println(one.getNextLoc(4,3));
     }
 }
